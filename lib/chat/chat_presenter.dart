d9871d1fc0c84d98fa0c9bd3437ca5dc1bfaa308
david <ariedov@gmail.com>
2018-10-13 22:07:08 +0300

    includes mqtt. listens to messages

--- lib/chat/chat_presenter.dart
+++ lib/chat/chat_presenter.dart
@@ -5,11 +5,15 @@ import 'package:screensee/chat/message.dart';
 import 'package:screensee/cookie.dart';
 import 'package:screensee/room.dart';
 
+import 'package:mqtt_client/mqtt_client.dart' as mqtt;
+
 class ChatPresenter {
   final CookieStorage cookieStorage;
   ChatView view;
 
   Room room;
+
+  mqtt.MqttClient client;
 
   ChatPresenter(this.cookieStorage);
 
@@ -24,9 +28,50 @@ class ChatPresenter {
 
       final messageJson = json.decode(response.body);
       view?.showChat(readArrayFromJson(messageJson["data"]));
+
+      _listenMqtt();
     } catch (e) {
       view?.showError();
     }
+  }
+
+  dispose() {
+    client?.disconnect();
+  }
+
+  _listenMqtt() async {
+    client = mqtt.MqttClient("185.143.145.119", '');
+    client.logging(true);
+    client.keepAlivePeriod = 30;
+
+    final mqtt.MqttConnectMessage connMess =
+        mqtt.MqttConnectMessage().keepAliveFor(30)
+        ..withClientIdentifier('Mqtt_MyClientUniqueId2')
+        .startClean()
+        .keepAliveFor(30)
+        .withWillTopic('willtopic')
+        .withWillMessage('My Will message')
+        .withWillQos(mqtt.MqttQos.atLeastOnce);
+    print('MQTT client connecting....');
+    client.connectionMessage = connMess;
+
+    try {
+      await client.connect();
+
+      client.updates.listen(_onMessage);
+
+      if (client.connectionState == mqtt.ConnectionState.connected) {
+        final topic = "room/${room.id}/message";
+        print('Subscribing to $topic');
+        client.subscribe(topic, mqtt.MqttQos.exactlyOnce);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _onMessage(List<mqtt.MqttReceivedMessage> event) {
+    print(event.toString());
   }
 
   void sendMessage(String message) async {
@@ -39,7 +84,7 @@ class ChatPresenter {
 
       final messageJson = json.decode(response.body);
       view?.addMessage(readFromJson(messageJson["data"]));
-    } catch (e) { 
+    } catch (e) {
       print(e);
     } finally {
       view?.hideMessageProgress();
